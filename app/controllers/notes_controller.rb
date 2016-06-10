7d5696a08e40030dc2fdc03fc8da7024450aa700
grek-andrian <gretsko.andrian@gmail.com>
2016-06-10 21:26:47 +0300

    Generate Notes  controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,36 @@
+class NotesController < ApplicationController
+
+  def index
+    #code
+  end
+
+  def show
+    #code
+  end
+
+  def create
+    #code
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+
+private
+
+def find_note
+  #code
+end
+
+def note_params
+  #code
+end
+end
